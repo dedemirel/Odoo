f7cdd9d407b80aa0b7ad250610c54820ab7de53f
beko2019 <bekiryokus@gmail.com>
2019-12-20 10:00:18 -0500

    commit

---
+++ src/test/java/odoo/pages/BasePage.java
@@ -0,0 +1,51 @@
+package odoo.pages;
+
+import odoo.utilities.BrowserUtils;
+import odoo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+//everything that is in common among odoo.pages
+//can go here
+//for example top menu elements don't belong to specific page
+//top menu appears on every single page
+//so we can keep them here
+public class BasePage {
+
+    @FindBy(css = "a[class='oe_menu_leaf']")
+    public WebElement pageSubTitle;
+
+    @FindBy(css = "#user-menu > a")
+    public WebElement userName;
+
+    @FindBy(linkText = "Logout")
+    public WebElement logOutLink;
+
+    @FindBy(linkText = "My User")
+    public WebElement myUser;
+
+    public BasePage() {
+        //this method requires to provide webdriver object for @FindBy
+        //and page class
+        //this means this page class
+        PageFactory.initElements(Driver.get(), this);
+    }
+    /**
+     * @return page name, for example: Dashboard
+     */
+    public String getPageSubTitle() {
+        //ant time we are verifying page name, or page subtitle, loader mask appears
+        BrowserUtils.wait(2);
+        BrowserUtils.waitForStaleElement(pageSubTitle);
+        return pageSubTitle.getText();
+    }
+
+}
