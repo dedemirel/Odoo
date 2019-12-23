ed20c47dd642950f0816c1ad9e550208841780b0
muhsin6161 <muhsin_mutlu61@hotmail.com>
2019-12-23 15:51:02 -0500

    Contacts module

--- src/test/java/odoo/pages/BasePage.java
+++ src/test/java/odoo/pages/BasePage.java
@@ -2,9 +2,14 @@ package odoo.pages;
 
 import odoo.utilities.BrowserUtils;
 import odoo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 //everything that is in common among odoo.pages
@@ -13,8 +18,10 @@ import org.openqa.selenium.support.PageFactory;
 //top menu appears on every single page
 //so we can keep them here
 public class BasePage {
+@FindBy(css = "[data-menu=\"68\"]")
+public WebElement contacts;
 
-    @FindBy(css = "a[class='oe_menu_leaf']")
+@FindBy(css = "a[class='oe_menu_leaf']")
     public WebElement pageSubTitle;
 
     @FindBy(css = "#user-menu > a")
@@ -25,6 +32,12 @@ public class BasePage {
 
     @FindBy(linkText = "My User")
     public WebElement myUser;
+@FindBy(css = "[class=\"o_loading\"]")
+    public WebElement loaderMask;
+    @FindBy(css = "[accesskey=\"c\"]")
+    public WebElement create;
+
+
 
     public BasePage() {
         //this method requires to provide webdriver object for @FindBy
@@ -38,8 +51,26 @@ public class BasePage {
     public String getPageSubTitle() {
         //ant time we are verifying page name, or page subtitle
         BrowserUtils.wait(2);
-        BrowserUtils.waitForStaleElement(pageSubTitle);
-        return pageSubTitle.getText();
+        BrowserUtils.waitForStaleElement(contacts);
+
+        return contacts.getText();
     }
+
+    public boolean waitUntilLoaderMaskDisappear() {
+        WebDriverWait wait = new WebDriverWait(Driver.get(), 30);
+        try {
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("[class=\"o_web_client oe_wait\"]")));
+            return true;
+        } catch (NoSuchElementException e) {
+            System.out.println("Loader mask not found!");
+            e.printStackTrace();
+            return true; // no loader mask, all good, return true
+        } catch (WebDriverException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
 
 }
