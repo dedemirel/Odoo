cc2eae6244f2a109076b6bf54c7fc8707e1f5115
serdarselcuk <serdarselcukus@outlook.com>
2019-12-27 14:31:12 -0500

    crm step def. : 1- I carry the page objects to Pages class under the utilities so that we will only create Pages object 2- redesign the page to read more easy crm features : 1- I created a background that will always executed before all scenarios so that I can add my scenarios 2- I made some changes in the scenarios to make them same with new login feature scenarios that I commited before

--- src/test/java/odoo/step_definitions/CrmStepDefinitions.java
+++ src/test/java/odoo/step_definitions/CrmStepDefinitions.java
@@ -1,46 +1,59 @@
 package odoo.step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
-import odoo.pages.CreatePage;
-import odoo.pages.LoginPage;
 import odoo.utilities.BrowserUtils;
 import odoo.utilities.Driver;
+import odoo.utilities.Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.Map;
 
 public class CrmStepDefinitions {
-LoginPage loginPage=new LoginPage();
-    CreatePage crt=new CreatePage();
-    @Then("use able to navigate {string} module")
-    public void use_able_to_navigate_module(String string) {
-        loginPage.navigateTo(string);
+    /* I create the objects on pages class under utils
+         so we don't need to create so much objets on all step definition pages. We can add
+         our page objects under the pages class and call them by creating only one objects in
+         this page def. classes*/
+Pages page = new Pages();
+
+    @And("user able to navigate {string} module")
+    public void userAbleToNavigateModule(String module) {
+
+        page.loginPage.navigateTo(module);
+
     }
+
 
     @Then("{string} title is displayed")
     public void title_is_displayed(String string) {
+
         BrowserUtils.wait(5);
-        loginPage.getPageSubTitle(loginPage.pipeline);
+        page.loginPage.getPageSubTitle(page.loginPage.pipeline);
     }
+
     @Then("user able to click {string} submodule")
     public void user_able_to_click_submodule(String string) {
- WebElement str=       Driver.get().findElement(By.xpath("//*[normalize-space()='"+string+"' and @type=\"button\"]"));
- str.click();
+
+        WebElement str = Driver.get().findElement(By.xpath("//*[normalize-space()='"+string+"' and @type=\"button\"]"));
+        str.click();
         System.out.println(str.isDisplayed());
     }
+
     @Then("user should open a page that has {string} text")
     public void user_should_open_a_page_that_has_text(String string) {
-BrowserUtils.wait(5);
+
+        BrowserUtils.wait(5);
         WebElement text=Driver.get().findElement(By.xpath("//*[normalize-space()='Create an Opportunity' and @class=\"modal-title\"]"));
         System.out.println(text.getText());
     }
 
     @Then("enter  on Opprtunity Title,Customer,Expected Revenue")
     public void enter_on_Opprtunity_Title_Customer_Expected_Revenue(Map<String,String> dataTable) {
-crt.createTo(dataTable.get("Opprtunity Title"),dataTable.get("Customer"),dataTable.get("Expected Revenue"));
+        page.crt.createTo(dataTable.get("Opprtunity Title"),dataTable.get("Customer"),dataTable.get("Expected Revenue"));
 
     }
+
 
 
 }
