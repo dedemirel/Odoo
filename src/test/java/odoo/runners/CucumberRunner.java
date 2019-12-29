0453e5700bcd732ef9ac145f0c17a14b4cc2e74a
dudemen15 <dursundemirel13@hotmail.com>
2019-12-29 01:37:18 -0500

    Events feature added

--- src/test/java/odoo/runners/CucumberRunner.java
+++ src/test/java/odoo/runners/CucumberRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "odoo/step_definitions",
-        dryRun = false,
-//        tags = "@Crm",
+        dryRun = true,
+        tags = "@Events",
         plugin = {"html:target/default-cucumber-reports",
                 "json:target/cucumber.json"
         }
