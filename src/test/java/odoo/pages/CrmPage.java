7d1c9b4743e29bab03792168fa0c4b9847510daf
serdarselcuk <serdarselcukus@outlook.com>
2020-01-11 04:23:36 -0600

    correctionOfColumnOfOpportunities => after moving opportunity checks if it is in the right column or not cleanDragAndDropTest => After DragAndDrop test it cleans the created opportunity kanbanColumnElement => return a WebElement that matches given int. used for droping opportunity notPresenceOfOpportunity => After deleted opportunity it makes sure there is nor any opportunities with that name opportunitiyLocator => it returns string of locator of oportunity deleteOpportunity => clickes to the delete button and than ok button under  the dropdown menu openDropDownMenuOnOpprtunities=> opens the dropDown menu on the right corner off opprtunity

--- src/test/java/odoo/pages/CrmPage.java
+++ src/test/java/odoo/pages/CrmPage.java
@@ -6,26 +6,23 @@ import odoo.utilities.ConfigurationReader;
 import odoo.utilities.Driver;
 import odoo.utilities.Pages;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import javax.swing.text.Utilities;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CrmPage extends BasePage {
     @FindBy(css = "[class *='o_kanban_group ']")
     public List<WebElement> listOfKanbanColumns;
 
-    private int numOfKanbanColumn = 1;
-    private By kanbanColumn =(By.cssSelector("[class *='o_kanban_group u' ]:nth-of-type("+numOfKanbanColumn+"))"));
-
-    private List<WebElement>elementsUnderKanbanColumn = Driver.get().findElements( By.cssSelector("[class *='o_kanban_group u' ]:nth-of-type("+numOfKanbanColumn+")>div[modifiers]"));
-
     @FindBy(css = " [class *='o_kanban_group u' ]>div[modifiers]:nth-child(2)")
-    public WebElement opportunityElement;
+    public WebElement opportunityElement;// it gives the first element in the first kanban kolumn wich is not empty
 
     @FindBy(xpath = "//*[normalize-space()='Create' and @type='button']")
     public WebElement createButton;
@@ -72,7 +69,84 @@ public class CrmPage extends BasePage {
     @FindBy(css = "[class='btn btn-primary btn-sm o_form_button_save']")
     public WebElement saveForActivity;
 
-  /*  public static void main(String[] args){
+    private Actions move = new Actions(Driver.get());
+
+    public CrmPage() {
+        PageFactory.initElements(Driver.get(), this);
+    }
+
+
+    public String opportunitiyLocator(String nameOfOpportunity){// we can find the opportunity element by text
+
+         return "//*[text() = '" + nameOfOpportunity + "']/parent::node()/parent::node()/parent::node()";
+
+    }
+
+
+    public boolean notPresenceOfOpportunity(String nameOfOpportunity){// if not exist returns true
+        try {
+            return BrowserUtils.waitForInVisibility(opportunitiyLocator(nameOfOpportunity), 1);
+        }catch (TimeoutException e){
+            return false;
+        }
+    }
+
+
+    public boolean checkListSize(String userName){
+
+        List<WebElement>pipeLinesOwners = Driver.get().findElements(By.cssSelector(".oe_kanban_content img[title = '"+userName+"']"));
+        List<WebElement>pipeLines = Driver.get().findElements(By.cssSelector(".oe_kanban_content img"));
+
+        return pipeLines.size() == pipeLinesOwners.size();
+    }
+
+
+    public boolean userOpportunityListSize(){
+        List<WebElement>pipeLines = Driver.get().findElements(By.cssSelector(".oe_kanban_content img"));
+        return pipeLines.size() > 1;
+    }
+
+
+    public String kanbanColumnElement(int num){
+
+        return "[class *='o_kanban_group u' ]:nth-of-type("+ num +")";
+    }
+
+    public boolean cleanDragAndDropTest(){
+        if(!notPresenceOfOpportunity("DragAndDropTest")){
+            openDropDownMenuOnOpprtunities("DragAndDropTest");
+            deleteOpportunity("DragAndDropTest");
+        }
+        return notPresenceOfOpportunity("DragAndDropTest");
+    }
+
+
+    public boolean correctionOfColumnOfOpportunities(int columnNumber, String nameOfOpportunity){
+        WebElement columnExpected = Driver.get().findElement(By.cssSelector(kanbanColumnElement(columnNumber)));
+        WebElement columnActual = Driver.get().findElement(By.xpath("//*[text() = '" + nameOfOpportunity + "']/parent::node()/parent::node()/parent::node()/parent::node()/parent::node()"));
+
+        return columnActual.hashCode() == columnExpected.hashCode();
+    }
+
+
+    public boolean openDropDownMenuOnOpprtunities(String opportunityName){
+        move.moveToElement(Driver.get().findElement(By.xpath("//*[text() = '"+opportunityName+"']/parent::node()/parent::node()/parent::node()/parent::node()/div"))).click().perform();
+        return true;
+    }
+
+    public void deleteOpportunity(String opportunityName){
+
+        Driver.get().findElement(By.xpath("//*[text() = '"+opportunityName+"']/parent::node()/parent::node()/parent::node()/parent::node()/div/ul/li[2]/a[text()='Delete']")).click();
+        String okButton = "//span[text()='Ok']";
+        BrowserUtils.waitForPresence(okButton,10);
+        BrowserUtils.waitForClickablility(Driver.get().findElement(By.xpath(okButton)),10);
+        Driver.get().findElement(By.xpath(okButton+"/parent::node()")).click();
+
+    }
+/*
+the main method below can be used to delete the opportunities on CRM page that we created while testing.
+
+    public static void main(String[] args){
         Pages page = new Pages();
         Driver.get().get(ConfigurationReader.getProperty("url"));
 
@@ -81,19 +155,27 @@ public class CrmPage extends BasePage {
 
         page.loginPage.login(userName, password);
 
-        String result= "";
         page.loginPage.navigateTo("CRM");
-        WebElement element = page.crmPage.opportunityElement;
-        BrowserUtils.waitForVisibility(element, 10);
-//        BrowserUtils.wait(3);
-        for (WebElement text: page.crmPage.elementsUnderKanbanColumn ) {
-            result += text.getText();
-        };
+
+        page.crmPage.waitUntilLoaderMaskDisappear();
+        Actions move = new Actions(Driver.get());
+
+        while(true) {
+            move.moveToElement(Driver.get().findElement(By.xpath("//*[text() = 'iphone']/parent::node()/parent::node()/parent::node()/parent::node()/div"))).click().perform();
+
+            Driver.get().findElement(By.xpath("//*[text() = 'iphone']/parent::node()/parent::node()/parent::node()/parent::node()/div/ul/li[2]/a[text()='Delete']")).click();
+
+            page.crmPage.waitUntilLoaderMaskDisappear();
+            BrowserUtils.waitForPresence("//span[text()='Ok']",10);
+            BrowserUtils.waitForClickablility(Driver.get().findElement(By.xpath("//span[text()='Ok']")),10);
+            Driver.get().findElement(By.xpath("//span[text()='Ok']/parent::node()")).click();
+            page.crmPage.waitUntilLoaderMaskDisappear();
+        }
 
 
-        System.out.println("result"+ result);
-    }*/
 
+    }
+*/
 
 }
 
